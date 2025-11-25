1fb90c896625104214b2af5b80a988d074328c66
Fariha tammi <farihatammi@gmail.com>
2025-11-25 00:41:24 -0500

    added a compilation failure

--- src/test/java/org/apache/commons/mail/EmailTest.java
+++ src/test/java/org/apache/commons/mail/EmailTest.java
@@ -19,7 +19,7 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
-import org.junit.After;
+import org.junit.After
 import org.junit.Before;
 import org.junit.Test;
 import org.subethamail.wiser.Wiser;
